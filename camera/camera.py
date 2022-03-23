38c4d36ed75a82f473ced3a3db39efe46291e515
florianfmmartin <florianfmmartin@gmail.com>
2022-03-23 14:41:58 -0400

    before product

--- camera/camera.py
+++ camera/camera.py
@@ -3,16 +3,19 @@ import cv2
 import numpy as np
 import RPi.GPIO as GPIO
 import time
+import os
 from websockets import connect
 
 async def take_photo():
     print("Button pressed...")
 
+    os.system("raspistill -o photo.jpg -vf -n -br 42")
+
     img = cv2.imread("photo.jpg")
     rows, cols, ch = img.shape
     f_s = 720
 
-    pts1 = np.float32([[0, rows/2], [cols, rows/2], [0, rows], [cols, rows]])
+    pts1 = np.float32([[845, 625], [1700, 635], [180, 1505], [2370, 1535]])
     pts2 = np.float32([[0, 0], [f_s, 0], [0, f_s], [f_s, f_s]])
 
     M = cv2.getPerspectiveTransform(pts1, pts2)
@@ -28,21 +31,11 @@ async def take_photo():
 
     print("Done. Waiting for next button press...\n")
 
-def button_callback(channel):
-    global last_time
-    now = time.time()
-    if (now - last_time) > 2:
+print("Press `e` to exit")
+
+while (True):
+    message = input()
+    if (message == "e"):
+        exit()
+    elif (message == ""):
         asyncio.run(take_photo())
-        last_time = now
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-last_time = time.time()
-
-GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback)
-
-message = input("Press enter to quit\n\n")
-GPIO.cleanup()
-
